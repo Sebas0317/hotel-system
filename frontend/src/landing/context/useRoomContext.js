dc5a549ea42ee34b11de92451c6a57a9f3d3aea6
Sebas0317 <sebastiansandoval12371@gmail.com>
2026-04-08 23:40:27 -0500

    Cleanup: remove unused landing folder, mockData.js, improve meta tags, ESLint passes clean

--- frontend/src/landing/context/useRoomContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { RoomInfo } from './RoomContext';
-
-export function useRoomContext() {
-  const ctx = useContext(RoomInfo);
-  if (!ctx) throw new Error('useRoomContext must be used within RoomContext');
-  return ctx;
-}
--- frontend/src/landing/data/index.js
+++
@@ -1,2 +0,0 @@
-export { adultsList, kidsList, sliderData, hotelRules } from './constants';
-export { roomData } from './rooms';
